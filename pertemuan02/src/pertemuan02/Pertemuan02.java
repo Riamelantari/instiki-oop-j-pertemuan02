06bc4bd9eebfb51b3c91ba5eb82b0cc343112365
riamelantari <email akun Github>
2025-03-11 21:57:00 +0800

    Pertemuan 2

---
+++ pertemuan02/src/pertemuan02/Pertemuan02.java
@@ -0,0 +1,24 @@
+package pertemuan02;
+
+/**
+ *
+ * @author Komang Ria Melantari
+ * tgl: 11 Maret 2025
+ */
+public class Pertemuan02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String nama = "Komang Ria Melantari";
+       String nim = "2301010457";
+       
+       System.out.println("Pertemuan 2 Java");
+       System.out.println ("===============");
+       System.out.println("Nama   : "+nama); 
+       System.out.println("NIM  : "+nim);
+       System.out.printf("Nama saya %s dengan NIM %s, saat ini sedang belajar pemrograman bahasa Java",nama,nim);
+    }
+    
+}
